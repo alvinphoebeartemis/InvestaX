9184e3bdb39084bebfe2987f8457c96993ee5d88
Alvin Phoebe Artemis <alvinphoebeartemis@gmail.com>
2020-09-07 20:37:12 +0800

    [Modify] Enable `albums` directory on public [Add] Create `/health` end-points

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const serveIndex = require('serve-index');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Router Files
+const indexRouter = require('./routes/index');
+const photoRouter = require('./routes/photos');
 
-var app = express();
+// Initial App Express
+const app = express();
 
-// view engine setup
+// View Engine Setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Other initialization
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'albums')));
 
+// Router
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/photos', photoRouter);
+app.use('/albums', express.static('albums'), serveIndex('albums', {'icons': true}))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
